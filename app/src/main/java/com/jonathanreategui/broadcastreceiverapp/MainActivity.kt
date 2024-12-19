e6861d09d18d23e98ed584ce75cdf8da88e0c7dc
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-12-19 11:28:06 -0500

    Adding bluetooth state receiver and optimizations

--- app/src/main/java/com/jonathanreategui/broadcastreceiverapp/MainActivity.kt
+++ app/src/main/java/com/jonathanreategui/broadcastreceiverapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.jonathanreategui.broadcastreceiverapp
 
+import android.bluetooth.BluetoothAdapter
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,6 +28,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jonathanreategui.broadcastreceiverapp.ui.theme.BroadcastReceiverAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,45 +38,113 @@ class MainActivity : ComponentActivity() {
         setContent {
             BroadcastReceiverAppTheme {
                 val context = LocalContext.current
-                var text by remember { mutableStateOf("Toggle airplane mode to see the event update.") }
-                DisposableEffect(context, Intent.ACTION_AIRPLANE_MODE_CHANGED) {
-                    val receiver = AirplaneModeReceiver(
-                        onEnabled = { text = "Airplane mode enabled." },
-                        onDisabled = { text = "Airplane mode disabled." }
-                    )
-                    receiver.register(context)
-                    onDispose {
-                        receiver.unRegister(context)
-                    }
-                }
+
+                var airplaneModeText by remember { mutableStateOf("Toggle airplane mode.") }
+                var bluetoothText by remember { mutableStateOf("Toggle bluetooth.") }
+
+                setupAirplaneModeReceiver(context, onAirplaneModeChanged = { status ->
+                    airplaneModeText = status
+                })
+
+                setupBluetoothReceiver(context, onBluetoothStatusChanged = { status ->
+                    bluetoothText = status
+                })
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Description(text = text)
+                    MainContent(
+                        airplaneModeText = airplaneModeText,
+                        bluetoothText = bluetoothText
+                    )
                 }
+            }
+        }
+    }
+
+    @Composable
+    private fun setupAirplaneModeReceiver(
+        context: Context,
+        onAirplaneModeChanged: (String) -> Unit
+    ) {
+        DisposableEffect(context, Intent.ACTION_AIRPLANE_MODE_CHANGED) {
+            val airplaneModeReceiver = AirplaneModeReceiver(
+                onEnabled = { onAirplaneModeChanged("Airplane mode: enabled.") },
+                onDisabled = { onAirplaneModeChanged("Airplane mode: disabled.") }
+            )
+            airplaneModeReceiver.register(context)
+            onDispose {
+                airplaneModeReceiver.unRegister(context)
+            }
+        }
+    }
+
+    @Composable
+    private fun setupBluetoothReceiver(
+        context: Context,
+        onBluetoothStatusChanged: (String) -> Unit
+    ) {
+        DisposableEffect(context, BluetoothAdapter.ACTION_STATE_CHANGED) {
+            val bluetoothReceiver = BluetoothStateReceiver(
+                onEnabled = { onBluetoothStatusChanged("Bluetooth: enabled.") },
+                onDisabled = { onBluetoothStatusChanged("Bluetooth: disabled.") }
+            )
+            bluetoothReceiver.register(context)
+            onDispose {
+                bluetoothReceiver.unRegister(context)
             }
         }
     }
 }
 
 @Composable
-fun Description(text: String, modifier: Modifier = Modifier) {
+fun AirplaneModeStatus(text: String) {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxWidth(),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = text,
-            modifier = modifier
-        )
+        Text(text = text)
+    }
+    Spacer(modifier = Modifier.height(16.dp))
+}
+
+@Composable
+fun BluetoothStatus(text: String) {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = text)
+    }
+    Spacer(modifier = Modifier.height(16.dp))
+}
+
+@Composable
+fun MainContent(
+    airplaneModeText: String,
+    bluetoothText: String
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AirplaneModeStatus(airplaneModeText)
+        BluetoothStatus(bluetoothText)
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainContentPreview() {
     BroadcastReceiverAppTheme {
-        Description("Android")
+        MainContent(
+            airplaneModeText = "Airplane mode disabled.",
+            bluetoothText = "Bluetooth is enabled."
+        )
     }
 }
